459a10a000c0fe2a58e146977297ee4d375c905c
Yemisi Abolanle Ajibulu <48490614+ajibulu@users.noreply.github.com>
2020-04-07 19:32:25 -0700

    adding changes

---
+++ estimator.js
@@ -0,0 +1,9 @@
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  return {
+    data: input,
+    impact: {},
+    severeimpact: {}
+  };
+};
+export default covid19ImpactEstimator;
